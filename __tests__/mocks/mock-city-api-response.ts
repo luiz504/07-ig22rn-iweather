5ce3fa29460734fc0b5595123e09bf0eaa8869aa
luiz bueno <luiz504@hotmail.com>
2024-04-28 20:23:20 -0300

    test: testing service return manipulation

---
+++ __tests__/mocks/mock-city-api-response.ts
@@ -0,0 +1,8 @@
+import { CityAPIResponse } from '@services/getCityByNameService'
+
+export const mockCityAPIResponse: CityAPIResponse = {
+  id: '1',
+  name: 'Brasilia',
+  coord: { lon: -47, lat: -15 },
+  sys: { country: 'BR' },
+}
