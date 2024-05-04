13b0128920faa6ad87cf18aa75602516b9d3e011
luiz bueno <luiz504@hotmail.com>
2024-05-04 16:23:24 -0300

    testing: config coverage

--- jest.config.js
+++ jest.config.js
@@ -6,14 +6,23 @@ module.exports = {
     "./__tests__/mocks/libs/async-storage.js",
     "./__tests__/mocks/libs/react-native-safe-area-context.js",
   ],
+  testPathIgnorePatterns: ["<rootDir>/__tests__/**"],
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
   ],
-
   transform: {
     "^.+\\.jsx?$": "babel-jest",
     "^.+\\.svg$": "jest-transformer-svg",
   },
-
   testPathIgnorePatterns: ["<rootDir>/__tests__/"],
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.{ts,tsx}",
+    "!<rootDir>/src/**/styles.ts",
+  ],
+  coveragePathIgnorePatterns: [
+    "<rootDir>/src/@types",
+    "<rootDir>/src/styles",
+    "<rootDir>/src/libs/dayjs",
+  ],
+  coverageDirectory: "<rootDir>/__tests__/coverage",
 };
